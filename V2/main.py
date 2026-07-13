699b2973fd22a84b763123a7d8d626421f369530
sanjaykrishna42006@gmail.com <sanjaykrishna42006@gmail.com>
2026-07-13 18:38:22 +0530

    Minor changes for plotting

--- V2/main.py
+++ V2/main.py
@@ -20,5 +20,23 @@ frame = np.array([(beam1, 20), (beam2, 20), (beam3, 30)])
 
 nodes, elements = mesh.OneDMeshing(frame)
 
+XY = np.array([list(node.coords) for node in nodes])
+IDs = np.array([node.ID for node in nodes]).reshape(-1, 1)
+XY = np.hstack((XY, IDs))
+print(XY[7])
 print(nodes[0].ID, nodes[0].coords)
 print(elements[2].ID, elements[2].node1.ID, elements[2].node2.ID)
+
+
+plt.figure(figsize=(8,4))
+
+plt.scatter(XY[:,0], XY[:,1], color='k')
+
+for (x, y, id) in XY:
+    plt.text(x, y, str(int(id)),
+             fontsize=8,
+             color='red')
+
+plt.axis("equal")
+plt.grid(True)
+plt.show()
